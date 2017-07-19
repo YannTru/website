8737041988b438b92a2787a0d4ea1e16e3190700
Yann Trudel <yann.trudel@genisysgc.com>
2017-07-19 08:29:16 -0400

    - Change image - Trying to make gulp work

--- gulpfile.js
+++ gulpfile.js
@@ -92,4 +92,6 @@ gulp.task('dev', ['browserSync', 'less', 'minify-css', 'minify-js'], function() 
     // Reloads the browser whenever HTML or JS files change
     gulp.watch('*.html', browserSync.reload);
     gulp.watch('js/**/*.js', browserSync.reload);
+    module.exports = gulp;
 });
+
