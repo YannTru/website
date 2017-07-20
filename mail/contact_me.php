1331462835e2dcc3f1c57dfdb5345e7a2ec11b0e
Yann Trudel <yann.trudel@genisysgc.com>
2017-07-20 07:03:25 -0400

    - First try on making the email work

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -14,13 +14,20 @@ $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
-	
+
+
+$host = "smtp.sparkpostmail.com";
+$username = "SMTP_Injection";
+$password = "4aca03698ab9ab1c17f882a68a05b451ab205deb";
+$port = "587";
+$smtp = mail::factory('smtp', array ('host' => $host, 'port' => $port, 'auth' => true, 'username' => $username, 'password' => $password));;
+
 // Create the email and send the message
 $to = 'yann@yanntrudel.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: yann@yanntrudel.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
+$headers = "From: noreply@yanntrudel.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";
+$mail = $smtp->($to,$headers,$email_subject,$email_body);
 return true;			
 ?>
